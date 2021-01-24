eac61b05dd61569da0e100411897572a8411ec1d
unknown <ushabj@gmail.com>
2021-01-24 21:24:37 +0530

    finally my god

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('thanks for everthing\n');
+  res.send('finall we finished\n');
 });
 
 app.listen(PORT, HOST);
